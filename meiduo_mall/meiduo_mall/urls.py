bae28d9fde51a96f9d68eba11af5c4be06663d9f
wqy <983665445@qq.com>
2019-05-08 15:56:55 +0800

    展示用户注册界面

--- meiduo_mall/meiduo_mall/urls.py
+++ meiduo_mall/meiduo_mall/urls.py
@@ -13,11 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from apps.views import log
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$',log),
+    # url(r'^$',log),
+    url(r'^',include('apps.users.urls',namespace='users'))
 ]
