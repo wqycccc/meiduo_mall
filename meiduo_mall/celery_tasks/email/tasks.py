889382886b6751fd83303db6f37bdfb07d59d364
wqy <983665445@qq.com>
2019-05-17 19:21:44 +0800

    重试的验证,

--- meiduo_mall/celery_tasks/email/tasks.py
+++ meiduo_mall/celery_tasks/email/tasks.py
@@ -1,10 +1,16 @@
 from django.core.mail import send_mail
+
+from apps.views import logger
 from celery_tasks.main import app
 
-@app.task
-def send_verify_email(subject,message,from_email,recipient_list,html_message):
-    send_mail(subject=subject,
-              message=message,
-              from_email=from_email,
-              recipient_list=recipient_list,
-              html_message=html_message)
+@app.task(bind = True,default_retry_delay=3,name='send_email')
+def send_verify_email(self,subject,message,from_email,recipient_list,html_message):
+    try:
+        send_mail(subject=subject,
+                  message=message,
+                  from_email=from_email,
+                  recipient_list=recipient_list,
+                  html_message=html_message)
+    except Exception as exc:
+        logger.error(exc)
+        raise self.retry(exc=exc,max_retries=3)
--- meiduo_mall/celery_tasks/sms/tasks.py
+++ meiduo_mall/celery_tasks/sms/tasks.py
@@ -1,9 +1,18 @@
 from apps.verifications.constant import YUNTONGXUN_EXPIRE_TIME
+from apps.views import logger
 from celery_tasks.main import app
 from libs.yuntongxun.sms import CCP
 
-
+# bind=True 是表示把任务自己传递过去,这样我们就可以在任务的第一个参数中,传递self
+# # 函数中的 self 就是 Task(任务)本身
 # 这个函数必须要经过 celery的实例对象的task装饰器装饰
-@app.task
-def send_sms_code(mobile,sms_code):
-    CCP().send_template_sms(mobile, [sms_code, YUNTONGXUN_EXPIRE_TIME], 1)
+# default_retry_delay 任务重试时间
+@app.task(bind = True,default_retry_delay=3,name='send_sms')
+def send_sms_code( self,mobile, sms_code):
+    try:
+        result = CCP().send_template_sms(mobile, [sms_code, YUNTONGXUN_EXPIRE_TIME], 1)
+        if result != 0:
+            raise Exception('下单失败')
+    except Exception as exc:
+        logger.error(exc)
+        raise self.retry(exc=exc,max_retries=3)
