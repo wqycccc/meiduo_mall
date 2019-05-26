bdac734d9b28d6261a1b946d11f7f9d5544493d6
wqy <983665445@qq.com>
2019-05-26 17:35:06 +0800

    展示商品页面购物车

--- meiduo_mall/apps/carts/urls.py
+++ meiduo_mall/apps/carts/urls.py
@@ -2,5 +2,6 @@ from  django.conf.urls import url
 from apps.carts import views
 urlpatterns = [
         url(r'^carts/$',views.CartsView.as_view(),name='info'),
-        url(r'^carts/selection/$',views.CartSelectAllView.as_view(),name='cartselectall')
+        url(r'^carts/selection/$',views.CartSelectAllView.as_view(),name='cartselectall'),
+        url(r'^carts/simple/$',views.CartsSimpleView.as_view(),name='cartssimple'),
 ]
--- meiduo_mall/apps/goods/urls.py
+++ meiduo_mall/apps/goods/urls.py
@@ -1,9 +1,13 @@
 from  django.conf.urls import url
 from apps.goods import views
 urlpatterns = [
+        # 商品列表
         url(r'^list/(?P<category_id>\d+)/(?P<page_num>\d+)/$',views.ListView.as_view(),name='list'),
+        # 热销数据
         url(r'^hot/(?P<category_id>\d+)/$',views.HotView.as_view(),name='hot'),
+        # 商品查询
         url(r'^detail/(?P<sku_id>\d+)/$', views.DetailView.as_view(), name='detail'),
+        # 统计
         url(r'^detail/visit/(?P<category_id>\d+)/$', views.CategoryVisitView.as_view(), name='visit'),
 
 ]
