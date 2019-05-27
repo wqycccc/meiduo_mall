dd402a3b7d3c717f058b1826d92de3ac235eca4f
wqy <983665445@qq.com>
2019-05-27 19:45:37 +0800

    事务保存订单数据

--- meiduo_mall/apps/orders/urls.py
+++ meiduo_mall/apps/orders/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from apps.orders import views
 
 urlpatterns=[
-    url(r'^orders/settlement/$',views.PlaceOrderView.as_view(),name='placeorder')
+    url(r'^orders/settlement/$',views.PlaceOrderView.as_view(),name='placeorder'),
+    url(r'^orders/commit/$',views.OrderCommitView.as_view(),name='commit'),
 ]
--- meiduo_mall/apps/orders/views.py
+++ meiduo_mall/apps/orders/views.py
@@ -1,3 +1,6 @@
+import json
+
+from decimal import Decimal
 from django import http
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
@@ -7,8 +10,11 @@ from django.views import View
 from django_redis import get_redis_connection
 
 from apps.goods.models import SKU
+from apps.orders.models import OrderInfo, OrderGoods
 from apps.users.models import Address
 from apps.views import logger
+from utils.response_code import RETCODE
+from utils.views import LoginRequiredJSONMixin
 
 
 class PlaceOrderView(LoginRequiredMixin,View):
@@ -62,3 +68,123 @@ class PlaceOrderView(LoginRequiredMixin,View):
         }
 
         return render(request, 'place_order.html', context=context)
+
+# 保存订单基本信息
+class OrderCommitView(LoginRequiredJSONMixin,View):
+    """
+    生成订单信息需要涉及到订单基本信息和订单商品信息,因为 订单基本信息订单商品信息
+    是1对n,所以先生成1(订单基本信息)的数据,再生成订单商品
+
+    1. 生成订单基本信息
+        1.1 必须是登陆用户才可以访问.获取用户信息
+        1.2 获取提交的地址信息
+        1.3 获取提交的支付方式
+        1.4 手动生成一个订单id 年月日时分秒+9位用户id
+        1.5 运费,总金额和总数量(初始化为0)
+        1.6 订单状态(由支付方式决定)
+    2. 生成订单商品信息
+        2.1 连接redis.获取redis中的数据
+        2.2 获取选中商品的id [1,2,3]
+        2.3 对id进行遍历
+            2.4 查询商品
+            2.5 库存量的判断
+            2.6 修改商品的库存和销量
+            2.7 累加总数量和总金额
+            2.8 保存订单商品信息
+            2.9 保存订单的总数量和总金额
+
+    """
+    def post(self,request):
+        # 1.生成订单基本信息
+        #     1.1 必须是登陆用户才可以访问.获取用户信息
+        user = request.user
+        #     1.2 获取提交的地址信息
+        data = json.loads(request.body.decode())
+        address_id = data.get('address_id')
+        try:
+            address = Address.objects.get(pk = address_id)
+        except Address.DoesNotExist:
+            return http.JsonResponse({'code':RETCODE.PARAMERR,'errmsg':'参数错误'})
+        #     1.3 获取提交的支付方式
+        pay_method = data.get('pay_method')
+        # 对支付方式进行一个判断
+        # 模型类里定义了支付方式
+        if not pay_method in [OrderInfo.PAY_METHODS_ENUM['CASH'],OrderInfo.PAY_METHODS_ENUM['ALIPAY']]:
+            return http.JsonResponse({'code':RETCODE.PARAMERR,'errmsg':'参数错误'})
+        #     1.4 手动生成一个订单id 年月日时分秒+9位用户id
+        # Y 年
+        # m 月
+        # d 日
+        # H 时
+        # M 分
+        # S 秒
+        from django.utils import timezone
+        order_id = timezone.now().strftime('%Y%m%d%H%M%S')+'%09d'%user.id
+        #     1.5 运费,总金额和总数量(初始化为0)
+        freight = Decimal('10.00')#运费
+        total_amount = Decimal('0')#总金额
+        total_count = 0
+        #     1.6 订单状态(由支付方式决定)
+        if pay_method == OrderInfo.PAY_METHODS_ENUM['CASH']:
+            # 货到付款
+            status = OrderInfo.ORDER_STATUS_ENUM['UNSEND']
+        else:
+            status = OrderInfo.ORDER_STATUS_ENUM['UNPAID']
+        from django.db import transaction
+        with transaction.atomic():
+
+            #1.创建事务回滚的点
+            save_point=transaction.savepoint()
+        #     保存订单基本信息
+            order = OrderInfo.objects.create(
+                order_id=order_id,
+                user=user,
+                address=address,
+                total_count=total_count,
+                total_amount=total_amount,
+                freight=freight,
+                pay_method=pay_method,
+                status=status
+            )
+
+            # 2. 生成订单商品信息
+            #     2.1 连接redis.获取redis中的数据
+            redis_conn = get_redis_connection('carts')
+            id_count = redis_conn.hgetall('carts_%s'%user.id)
+            selected_ids = redis_conn.smembers('selected_%s'%user.id)
+
+            selected_carts = {}
+            for id in selected_ids:
+                selected_carts[int(id)] = int(id_count[id])
+            #     2.2 获取选中商品的id [1,2,3]
+            ids = selected_carts.keys()
+            #     2.3 对id进行遍历
+            for id in ids:
+            #         2.4 查询商品
+                sku = SKU.objects.get(pk=id)
+            #         2.5 库存量的判断
+                count = selected_carts[sku.id]
+                if sku.stock < count:
+                    # 出现问题进行回滚
+                    transaction.savepoint_rollback(save_point)
+                    # 说明库存不足
+                    return http.JsonResponse({'code':RETCODE.STOCKERR,'errmsg':'库存不足'})
+
+                #         2.6 修改商品的库存和销量
+                sku.stock -= count
+                sku.sales += count
+                sku.save()
+                #         2.7 累加总数量和总金额
+                #         2.8 保存订单商品信息
+                order = OrderGoods.objects.create(
+                    order=order,
+                    sku = sku,
+                    count = count,
+                    price =sku.price,
+                )
+     #      2.9 保存订单的总数量和总金额
+            order.save()
+            # 3.提交
+            transaction.savepoint_commit(save_point)
+            # 返回响应
+            return http.JsonResponse({'code':RETCODE.OK,'errmsg':'下单成功','order':order.order_id})
