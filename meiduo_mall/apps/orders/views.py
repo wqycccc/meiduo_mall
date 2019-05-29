4d009a4b8d10f71b9d25fa4427ceb45ce14e3a7e
wqy <983665445@qq.com>
2019-05-29 15:56:04 +0800

    乐观锁

--- meiduo_mall/apps/orders/views.py
+++ meiduo_mall/apps/orders/views.py
@@ -17,23 +17,22 @@ from utils.response_code import RETCODE
 from utils.views import LoginRequiredJSONMixin
 
 
-class PlaceOrderView(LoginRequiredMixin,View):
-
-    def get(self,request):
+class PlaceOrderView(LoginRequiredMixin, View):
+    def get(self, request):
         # 获取用户信息
         user = request.user
         # 查询地址信息
         try:
-            addresses = Address.objects.filter(user =user,is_deleted=False)
+            addresses = Address.objects.filter(user=user, is_deleted=False)
         except Exception as e:
             logger.error(e)
             return http.HttpResponseBadRequest('未找到数据')
         # 链接redis
         redis_conn = get_redis_connection('carts')
         # hash
-        sku_id_count = redis_conn.hgetall('carts_%s'%user.id)
+        sku_id_count = redis_conn.hgetall('carts_%s' % user.id)
         # set
-        ids = redis_conn.smembers('selected_%s'%user.id)
+        ids = redis_conn.smembers('selected_%s' % user.id)
         # 取出的商品是base类型,要进行强制转换为int类型
         selected_carts = {}
         for id in ids:
@@ -43,10 +42,10 @@ class PlaceOrderView(LoginRequiredMixin,View):
         # pk__in 表示查询在ids中的数据
         skus = SKU.objects.filter(pk__in=ids)
         # 对象商品列表进行遍历
-        total_count =  0#总数量
+        total_count = 0  # 总数量
         # 导入货比类型
         from decimal import Decimal
-        total_amount = Decimal('0')#总金额
+        total_amount = Decimal('0')  # 总金额
         for sku in skus:
             # 8.遍历的过程中 对sku添加数量和对应商品的总金额
             sku.count = selected_carts[sku.id]  # 数量小计
@@ -69,8 +68,9 @@ class PlaceOrderView(LoginRequiredMixin,View):
 
         return render(request, 'place_order.html', context=context)
 
+
 # 保存订单基本信息
-class OrderCommitView(LoginRequiredJSONMixin,View):
+class OrderCommitView(LoginRequiredJSONMixin, View):
     """
     生成订单信息需要涉及到订单基本信息和订单商品信息,因为 订单基本信息订单商品信息
     是1对n,所以先生成1(订单基本信息)的数据,再生成订单商品
@@ -94,7 +94,8 @@ class OrderCommitView(LoginRequiredJSONMixin,View):
             2.9 保存订单的总数量和总金额
 
     """
-    def post(self,request):
+
+    def post(self, request):
         # 1.生成订单基本信息
         #     1.1 必须是登陆用户才可以访问.获取用户信息
         user = request.user
@@ -102,16 +103,16 @@ class OrderCommitView(LoginRequiredJSONMixin,View):
         data = json.loads(request.body.decode())
         address_id = data.get('address_id')
         try:
-            address = Address.objects.get(pk = address_id)
+            address = Address.objects.get(pk=address_id)
         except Address.DoesNotExist:
-            return http.JsonResponse({'code':RETCODE.PARAMERR,'errmsg':'参数错误'})
-        #     1.3 获取提交的支付方式
+            return http.JsonResponse({'code': RETCODE.PARAMERR, 'errmsg': '参数错误'})
+        # 1.3 获取提交的支付方式
         pay_method = data.get('pay_method')
         # 对支付方式进行一个判断
         # 模型类里定义了支付方式
-        if not pay_method in [OrderInfo.PAY_METHODS_ENUM['CASH'],OrderInfo.PAY_METHODS_ENUM['ALIPAY']]:
-            return http.JsonResponse({'code':RETCODE.PARAMERR,'errmsg':'参数错误'})
-        #     1.4 手动生成一个订单id 年月日时分秒+9位用户id
+        if not pay_method in [OrderInfo.PAY_METHODS_ENUM['CASH'], OrderInfo.PAY_METHODS_ENUM['ALIPAY']]:
+            return http.JsonResponse({'code': RETCODE.PARAMERR, 'errmsg': '参数错误'})
+        # 1.4 手动生成一个订单id 年月日时分秒+9位用户id
         # Y 年
         # m 月
         # d 日
@@ -119,10 +120,10 @@ class OrderCommitView(LoginRequiredJSONMixin,View):
         # M 分
         # S 秒
         from django.utils import timezone
-        order_id = timezone.now().strftime('%Y%m%d%H%M%S')+'%09d'%user.id
+        order_id = timezone.now().strftime('%Y%m%d%H%M%S') + '%09d' % user.id
         #     1.5 运费,总金额和总数量(初始化为0)
-        freight = Decimal('10.00')#运费
-        total_amount = Decimal('0')#总金额
+        freight = Decimal('10.00')  # 运费
+        total_amount = Decimal('0')  # 总金额
         total_count = 0
         #     1.6 订单状态(由支付方式决定)
         if pay_method == OrderInfo.PAY_METHODS_ENUM['CASH']:
@@ -133,9 +134,9 @@ class OrderCommitView(LoginRequiredJSONMixin,View):
         from django.db import transaction
         with transaction.atomic():
 
-            #1.创建事务回滚的点
-            save_point=transaction.savepoint()
-        #     保存订单基本信息
+            # 1.创建事务回滚的点
+            save_point = transaction.savepoint()
+            #     保存订单基本信息
             order = OrderInfo.objects.create(
                 order_id=order_id,
                 user=user,
@@ -150,41 +151,58 @@ class OrderCommitView(LoginRequiredJSONMixin,View):
             # 2. 生成订单商品信息
             #     2.1 连接redis.获取redis中的数据
             redis_conn = get_redis_connection('carts')
-            id_count = redis_conn.hgetall('carts_%s'%user.id)
-            selected_ids = redis_conn.smembers('selected_%s'%user.id)
+            id_count = redis_conn.hgetall('carts_%s' % user.id)
+            selected_ids = redis_conn.smembers('selected_%s' % user.id)
 
             selected_carts = {}
             for id in selected_ids:
                 selected_carts[int(id)] = int(id_count[id])
-            #     2.2 获取选中商品的id [1,2,3]
+            # 2.2 获取选中商品的id [1,2,3]
             ids = selected_carts.keys()
             #     2.3 对id进行遍历
             for id in ids:
-            #         2.4 查询商品
+                #         2.4 查询商品
                 sku = SKU.objects.get(pk=id)
-            #         2.5 库存量的判断
+                #         2.5 库存量的判断
                 count = selected_carts[sku.id]
                 if sku.stock < count:
                     # 出现问题进行回滚
                     transaction.savepoint_rollback(save_point)
                     # 说明库存不足
-                    return http.JsonResponse({'code':RETCODE.STOCKERR,'errmsg':'库存不足'})
+                    return http.JsonResponse({'code': RETCODE.STOCKERR, 'errmsg': '库存不足'})
 
-                #         2.6 修改商品的库存和销量
-                sku.stock -= count
-                sku.sales += count
-                sku.save()
+                    # 2.6 修改商品的库存和销量
+                # sku.stock -= count
+                # sku.sales += count
+                # sku.save()
+                import time
+                time.sleep(5)
+                # 2.6.1  乐观锁
+                # 先获取之前的库存
+                old_stock = sku.stock
+                new_stock = sku.stock - count  # 新库存
+                new_sales = sku.sales + count  #新销量
+                rect = SKU.objects.filter(id = id,stock=old_stock).update(
+                    stock = new_stock,
+                    sales = new_sales
+                )
+                if rect == 0:
+                    # 下单失败
+                    transaction.savepoint_rollback(save_point)
+                    return http.JsonResponse({'code':RETCODE.STOCKERR,'errmsg':'下单失败'})
+                else:
+                    print('下单成功')
                 #         2.7 累加总数量和总金额
                 #         2.8 保存订单商品信息
                 order = OrderGoods.objects.create(
                     order=order,
-                    sku = sku,
-                    count = count,
-                    price =sku.price,
+                    sku=sku,
+                    count=count,
+                    price=sku.price,
                 )
-     #      2.9 保存订单的总数量和总金额
+                #      2.9 保存订单的总数量和总金额
             order.save()
             # 3.提交
             transaction.savepoint_commit(save_point)
             # 返回响应
-            return http.JsonResponse({'code':RETCODE.OK,'errmsg':'下单成功','order':order.order_id})
+            return http.JsonResponse({'code': RETCODE.OK, 'errmsg': '下单成功', 'order': order.order_id})
