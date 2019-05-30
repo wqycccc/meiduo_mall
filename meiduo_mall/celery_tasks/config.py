e8d0f0624d7ff893a3195b343f6b317f09981b16
wqy <983665445@qq.com>
2019-05-30 19:23:48 +0800

    RabbitMQ

--- meiduo_mall/celery_tasks/config.py
+++ meiduo_mall/celery_tasks/config.py
@@ -1,4 +1,5 @@
-# boker(信息队列)的配置信息存放到redis的14号数据库
-broker_url = "redis://127.0.0.1/14"
-# 执行结果存放到redis15号数据库
-result_backend = "redis://127.0.0.1/15"
+# # boker(信息队列)的配置信息存放到redis的14号数据库
+# broker_url = "redis://127.0.0.1/14"
+# # 执行结果存放到redis15号数据库
+# result_backend = "redis://127.0.0.1/15"
+broker_url= 'amqp://guest:guest@127.0.0.1:5672'
